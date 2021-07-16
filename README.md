5f2161f58d0d9916110e3a004ecf8450fc969a06
mechanicalmatt <matt.ta.gray@gmail.com>
2021-07-16 00:07:54 -0700

    functionality - isolate chess username from tweets

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# chess
+MVP:
+-   player mentions @chess_bot and the bot replies with their stats
+
+MVP BREAKDOWN:
+-   fetch player data
+--      1. rank in each game type (D, B, R, B)
+
+####
+
+FURTHER FEATURES:
+ 
+give more detail on a profile
+--      analyze fav openings
+--      matplotlib charts/graphs on their history
+basic statistics
+player comparison
+--      x vs. y: win/lose odds
+--      rankings
+
+####
+
+make the usability as simple as possible - very simple formatting
+**      stats "username"
+**      daily stats "username"
+**      rapid stats "username"
+**      ...
+
+####
+
+INTERACTIONS:
+
+how to tell if tweet has already been replied to:
+**  send reply then like (in that order, in the same function)
+**  only like tweets that have been processed -
+**  "if favorited == True: ignore"
--- main.py
+++ main.py
@@ -1,2 +1,123 @@
-print('test')
-print('test')
+import urllib.request
+import ast
+import tweepy
+import time
+
+CONSUMER_KEY = 'EcceTMfViq54zGJrRvrdIUru9'
+CONSUMER_SECRET = 'FJKgoWoowQ2T0m7dg8t466ql8O0PbAQLyBHqts3zrU0xaLpik0'
+ACCESS_KEY = '1409557697662316545-CJtJ4MoqtJY8NEhdqERv3sx3eaGdcr'
+ACCESS_SECRET = 'fxDdpCrU2OkiTdiYv89IhiH2ouvrlY7DBw74uM1EY4bPR'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+user = api.me()
+mentions = api.mentions_timeline()
+
+####
+
+def playerProfile(username):
+    webUrl = urllib.request.urlopen('https://api.chess.com/pub/player/{}'.format(username))
+    data = str(webUrl.read())
+    split_data = data.split(',')
+    print(data)
+    for i in split_data:
+        print(i)
+
+# playerProfile('mechmartian')
+
+import urllib.request
+import ast
+
+# print stats for all gametypes of a given gametype
+def stats(username):
+    webUrl = urllib.request.urlopen('https://api.chess.com/pub/player/{}/stats'.format(username))
+    data = webUrl.read()
+    a = ast.literal_eval(data.decode('utf-8'))
+    print(username + ' stats:')
+    print('Daily rating - ' + str(a['chess_daily']['last']['rating']))
+    print('Rapid rating - ' + str(a['chess_rapid']['last']['rating']))
+    print('Bullet rating - ' + str(a['chess_bullet']['last']['rating']))
+    print('Blitz rating - ' + str(a['chess_blitz']['last']['rating']))
+def statsDaily(username):
+    webUrl = urllib.request.urlopen('https://api.chess.com/pub/player/{}/stats'.format(username))
+    data = webUrl.read()
+    a = ast.literal_eval(data.decode('utf-8'))
+    print('{} daily rating - '.format(username) + str(a['chess_daily']['last']['rating']))
+def statsRapid(username):
+    webUrl = urllib.request.urlopen('https://api.chess.com/pub/player/{}/stats'.format(username))
+    data = webUrl.read()
+    a = ast.literal_eval(data.decode('utf-8'))
+    print('{} rapid rating - '.format(username) + str(a['chess_rapid']['last']['rating']))
+def statsBullet(username):
+    webUrl = urllib.request.urlopen('https://api.chess.com/pub/player/{}/stats'.format(username))
+    data = webUrl.read()
+    a = ast.literal_eval(data.decode('utf-8'))
+    print('{} bullet rating - '.format(username) + str(a['chess_bullet']['last']['rating']))
+def statsBlitz(username):
+    webUrl = urllib.request.urlopen('https://api.chess.com/pub/player/{}/stats'.format(username))
+    data = webUrl.read()
+    a = ast.literal_eval(data.decode('utf-8'))
+    print('{} blitz rating - '.format(username) + str(a['chess_blitz']['last']['rating']))
+    
+stats('mechmartian')
+
+########
+
+def leaderboardDaily():
+    webUrl = urllib.request.urlopen('https://api.chess.com/pub/leaderboards')
+    data = webUrl.read()
+    a = ast.literal_eval(data.decode('utf-8'))
+    print(a.keys())
+    for i in a['daily']:
+        if i['rank'] < 7:
+            print(i['rank'], i['username'])
+
+# leaderboardDaily()
+
+####
+
+def playerGames(username):
+    webUrl = urllib.request.urlopen('https://api.chess.com/pub/player/{}/games'.format(username))
+    data = webUrl.read()
+    a = ast.literal_eval(data.decode('utf-8'))
+    print(a)
+    
+# playerGames('mechmartian')
+
+# def playerInfo(username):
+#     webUrl = urllib.request.urlopen('https://www.chess.com/stats/openings/{}'.format(username))
+#     data = webUrl.read()
+#     a = ast.literal_eval(data.decode('utf-8'))
+#     print(a)
+#
+# playerInfo('mechmartian')
+
+def reply(username):
+    api.update_status('@' + str(username) + playerStats)
+
+# get @ from a mentions
+for i in mentions:
+    a = i.__dict__['author'].__dict__['screen_name']
+    # print('@' + a)
+
+# list of tweet_id for mentions already replied to
+replied = []
+# get text from mentions and isolate chessUsername
+def get_username():
+    chess_username = []
+    t0 = mentions[0]
+    replied.append(mentions[0].__dict__['id'])
+    for i in t0.__dict__['text'].split(' '):
+        if '!' in i:
+            chess_username = i.strip('!')
+    return chess_username
+
+def send_reply():
+    user = get_username()
+    t0 = mentions[0]
+    print(t0.__dict__['id'])
+    if t0.__dict__['id'] in replied:
+        print('replying...')
+    print('b', replied)
+send_reply()
